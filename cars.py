71a895abb7158214f0697ee6cc3cd3c7702bbcfd
bnkala <nkala.bryan3@gmail.com>
2023-01-15 16:56:12 +0200

    Cars sold

---
+++ cars.py
@@ -0,0 +1,20 @@
+if __name__=="__main__":
+    cars = []
+    numCars = 0
+    num =1
+    totCars =0
+    highNum =0
+    high =0
+    i = 0
+    for i in range(14):
+        numCars = int(input("Enter number of Car(s) sold: "))
+        cars.append(numCars)
+        if(cars[i]> high):
+            high = cars[i]
+            highNum = num
+        
+        num+=1
+        totCars += cars[i]
+    
+    print("The total number of Cars sold: {}".format(totCars))
+    print("The Salesperson with max. number of Slaes: {}".format(highNum))
